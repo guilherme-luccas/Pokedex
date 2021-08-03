19882e8c783e4f7476906968dddc941426bd38ee
Guilherme Luccas <guilherme.luccas@outlook.com>
2021-08-03 17:16:20 -0300

    1

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -5,7 +5,7 @@ import { SearchContext } from "../../SearchContext";
 type Props = {
   image: string;
   name: string;
-  types: [];
+  types: [{}];
 };
 
 export function CardGrid({ image, name, types }: Props) {
