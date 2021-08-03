1ebd10a399aac8efdc2ce6a8d9323a2ee7aed2f5
Guilherme Luccas <guilherme.luccas@outlook.com>
2021-08-03 18:00:45 -0300

    1

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -56,6 +56,7 @@ export function Grid() {
           .map((pokemon: any, index) => {
             return (
               <div
+                key={index}
                 data-index={index}
                 onClick={() => {
                   ZoomIn(pokemon);
